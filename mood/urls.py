436c088bc6551da184ddf238b12bcdba4f0775a1
Zion Keretho <natekrth@gmail.com>
2022-10-26 23:04:58 +0700

    add header bar to each page

--- mood/urls.py
+++ mood/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('')
+    path('', views.welcome, name='welcome'),
+    path('mood', views.mood, name='mood'),
+    path('mood/record', views.record, name='record'),
+    path('mood/dailymood', views.daily_mood, name='dailymood'),
+    path('mood/discover', views.discover, name='discover'),
+    path('mood/profile', views.profile, name='profile'),
 ]
