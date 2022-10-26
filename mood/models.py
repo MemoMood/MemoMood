d47bb1381c3a87d8481ecfc3b2ed5080c6c40ae7
sivazzer <sivakorn.ru@ku.th>
2022-10-27 01:31:49 +0700

    add models

--- mood/models.py
+++ mood/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MoodFactors(models.Model):
+    factor = models.CharField(
+        max_length=255, unique=True, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.factor
+
+
+class Detail(models.Model):
+    factor = models.ForeignKey(MoodFactors, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TimeSelection(models.Model):
+    start = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    end = models.DateTimeField(blank=True, null=True)
+
+    def duration(self):
+        if not self.end:
+            return 0
+        return self.end - self.start
+
+
+class Diary(models.Model):
+    text = models.TextField()
