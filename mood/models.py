0a35983bafcf34aaf06f9f477705aca3dc488e4e
panitnt <panittatanya@gmail.com>
2022-10-28 14:04:24 +0700

    implemented model

--- mood/models.py
+++ mood/models.py
@@ -11,23 +11,28 @@ class MoodFactors(models.Model):
         return self.factor
 
 
-class Detail(models.Model):
+class FactorDetail(models.Model):
     factor = models.ForeignKey(MoodFactors, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=255, unique=True,
+                            null=False, blank=False)
 
     def __str__(self) -> str:
         return self.name
 
 
-class TimeSelection(models.Model):
-    start = models.DateTimeField(auto_now_add=True, blank=False, null=False)
-    end = models.DateTimeField(blank=True, null=True)
-
-    def duration(self):
-        if not self.end:
-            return 0
-        return self.end - self.start
-
-
 class Diary(models.Model):
-    text = models.TextField()
+    time = models.DateTimeField(blank=False, null=False)
+    mood = models.CharField(max_length=255, blank=False, null=False)
+    place = models.CharField(max_length=255, blank=False, null=False)
+    weather = models.CharField(max_length=255, blank=False)
+    text = models.TextField(blank=True, null=True)
+    people = models.ManyToManyField(FactorDetail)
+
+    def __str__(self) -> str:
+        return f'Diary at {self.time}'
+
+
+class SleepTimeField(models.Model):
+    day = models.DateField(null=False, blank=False)
+    hour = models.DecimalField(
+        null=False, blank=False, max_digits=2, decimal_places=2)
