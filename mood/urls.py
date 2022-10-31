c574a13dce6191ae4cf37f13a5302031c829317d
panitnt <panittatanya@gmail.com>
2022-11-01 00:50:59 +0700

    add set sleep time

--- mood/urls.py
+++ mood/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('mood', views.mood, name='mood'),
-    # path('mood/sleep_time', views.set_sleep_time, name='set_sleep_time'),
+    path('mood/sleep_time', views.set_sleep_time, name='set_sleep_time'),
     path('mood/record', views.record, name='record'),
     path('mood/accept/record', views.accept_record, name='accept_record'),
     path('mood/accept/place', views.accept_place, name='accept_place'),
