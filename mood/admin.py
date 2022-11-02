4632cc6e09109426ea205473c356d9f11667e937
panitnt <panittatanya@gmail.com>
2022-11-03 02:42:21 +0700

    add register to admin

--- mood/admin.py
+++ mood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MoodFactors, FactorDetail, Diary, SleepTimeField
 
 # Register your models here.
+admin.site.register(MoodFactors)
+admin.site.register(FactorDetail)
+admin.site.register(Diary)
+admin.site.register(SleepTimeField)
