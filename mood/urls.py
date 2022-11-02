8e986a2cab3ec3e2cdd66f827e5acde3fadc0227
panitnt <panittatanya@gmail.com>
2022-11-03 03:07:07 +0700

    implemented add positive mood

--- mood/urls.py
+++ mood/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('mood/accept/adding', views.accept_adding, name='accept_adding'),
     path('mood/add_place', views.add_place, name='add_place'),
     path('mood/add_people', views.add_people, name='add_people'),
+    path('mood/add_mood/positive', views.add_mood_positive, name='add_mood_positive'),
     path('mood/dailymood', views.daily_mood, name='daily_mood'),
     path('mood/dailymood/show', views.daily_mood_show, name='daily_mood_show'),
     path('mood/discover', views.discover, name='discover'),
