f12a344d2120ca8423e73be92c3fc0359e0cbb7c
panitnt <panittatanya@gmail.com>
2022-11-07 13:29:48 +0700

    create page and add styles

--- discover/views.py
+++ discover/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from mood.models import MoodFactors, FactorDetail, SleepTimeField
 
 # Create your views here.
 def index_discover(request):
     return render(request, 'discover/index.html')
+
+def mood_with_place(request):
+    return render(request, 'discover/place.html')
+
+def mood_with_sleep_time(request):
+    return render(request, 'discover/sleep_time.html')
