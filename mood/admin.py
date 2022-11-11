9004abe7f42ae7b7d8fb69003ce3bef239735df7
panitnt <panittatanya@gmail.com>
2022-11-11 10:23:21 +0700

    Add UserDiary to admin

--- mood/admin.py
+++ mood/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import MoodFactors, FactorDetail, Diary, SleepTimeField
+from .models import MoodFactors, FactorDetail, Diary, SleepTimeField, UserDiary
 
 # Register your models here.
 admin.site.register(MoodFactors)
 admin.site.register(FactorDetail)
 admin.site.register(Diary)
 admin.site.register(SleepTimeField)
+admin.site.register(UserDiary)
