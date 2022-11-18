b35f79e9236b638e371c06c3dd3354fdfaa26f83
teenteennnii <teennopwijit@gmail.com>
2022-11-18 17:23:51 +0700

    update MeMoodModelTest

--- mood/tests.py
+++ mood/tests.py
@@ -10,6 +10,8 @@ class MeMoodModelTest(TestCase):
         self.time_test = "2022-11-14"
         self.mood_factor = MoodFactors(factor="happy")
         self.factor_detail = FactorDetail(name="Alpha", category="Positive", detail="Main", factor=self.mood_factor, favorite="True")
+        self.diary = Diary(time=self.time_test, place="KU", weather="Sunny", text="Hello")
+        self.sleep_time = SleepTimeField(user=User(username="Harry"), day="2022-11-14", hour="8")
 
     def test_mood_factors(self):
         self.assertEqual("happy", self.mood_factor.factor)
@@ -24,22 +26,28 @@ class MeMoodModelTest(TestCase):
         self.assertEqual("True", self.factor_detail.favorite)
 
     def test_diary(self):
-        diary = Diary(time=self.time_test, place="KU", weather="Sunny", text="Hello")
         with self.assertRaises(ValueError):
-            diary.mood.add(self.factor_detail.pk)
-        self.assertEqual("2022-11-14", diary.time)
-        self.assertEqual("KU", diary.place)
-        self.assertEqual("Sunny", diary.weather)
-        self.assertEqual("Hello", diary.text)
-        self.assertEqual("Diary at 2022-11-14", str(diary))
+            self.diary.mood.add(self.factor_detail.pk)
+        self.assertEqual("2022-11-14", self.diary.time)
+        self.assertEqual("KU", self.diary.place)
+        self.assertEqual("Sunny", self.diary.weather)
+        self.assertEqual("Hello", self.diary.text)
+        self.assertEqual("Diary at 2022-11-14", str(self.diary))
 
     def test_sleep_time_field(self):
-        sleep_time = SleepTimeField(day="2022-11-14", hour="8")
-        self.assertEqual("2022-11-14 sleep 8 hour", str(sleep_time))
+        self.assertEqual(User, type(self.sleep_time.user))
+        self.assertEqual("Harry", self.sleep_time.user.username)
+        self.assertEqual("2022-11-14", self.sleep_time.day)
+        self.assertEqual("8", self.sleep_time.hour)
+        self.assertEqual("2022-11-14 sleep 8 hour", str(self.sleep_time))
 
     def test_user_diary(self):
-        user_diary = UserDiary(user=User(username="David"))
-        self.assertEqual("User diary name: David", str(user_diary))
+        user_diary = UserDiary(user=User(username="Hermione"))
+        with self.assertRaises(ValueError):
+            user_diary.diary.add(diary=self.diary)
+            user_diary.factor.add(factor=self.factor_detail)
+            user_diary.sleep_time.add(sleeptime=self.sleep_time)
+        self.assertEqual("User diary name: Hermione", str(user_diary))
 
 
 class MeMoodViewsTest(TestCase):
