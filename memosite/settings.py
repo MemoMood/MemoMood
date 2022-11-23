b0cf5f297c6c42aba46042df6cd9abcd507a83cb
panitnt <panittatanya@gmail.com>
2022-11-23 14:24:35 +0700

    Remove GitHub login from issue#54

--- memosite/settings.py
+++ memosite/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.github',
     
     'crispy_forms',
 ]
