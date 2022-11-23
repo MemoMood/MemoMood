497c72ec910f8930bf5485981cacee6057942ce9
teenteennnii <teennopwijit@gmail.com>
2022-11-23 21:41:42 +0700

    change debug to false

--- memosite/settings.py
+++ memosite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-81c$8@d1j+pue(30pam_)q!es$jwit$z60&urpk0&m%_rl_j^v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
