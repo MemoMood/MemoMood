6a43782a3035d9dd0bd83dad0462073c28591ef2
Zion Keretho <natekrth@gmail.com>
2022-11-29 11:57:16 +0700

    add staticfile storage

--- memosite/settings.py
+++ memosite/settings.py
@@ -156,6 +156,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
