6c4e840b8b5c6315b7573bc2ac4370616449aa84
Zion Keretho <natekrth@gmail.com>
2022-11-29 13:48:58 +0700

    static root

--- memosite/settings.py
+++ memosite/settings.py
@@ -155,7 +155,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
