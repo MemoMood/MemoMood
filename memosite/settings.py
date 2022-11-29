6dac99d34959246546d3dd7cef563eeae52ab51c
Zion Keretho <natekrth@gmail.com>
2022-11-29 22:13:01 +0700

    update

--- memosite/settings.py
+++ memosite/settings.py
@@ -14,7 +14,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-# import django_heroku
+if 'HEROKU' in os.environ:
+    import django_heroku
+    django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
