8a8146c145cd2bfab1bfa2f4ed664d15f79648db
teenteennnii <teennopwijit@gmail.com>
2022-11-30 00:39:11 +0700

    update

--- memosite/settings.py
+++ memosite/settings.py
@@ -14,9 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-if 'HEROKU' in os.environ:
-    import django_heroku
-    django_heroku.settings(locals())
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,5 +158,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-# django_heroku.settings(locals())
 django_heroku.settings(locals())
