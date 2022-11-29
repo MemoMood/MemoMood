7776dec843e4fb95db77f417cad008b459447765
teenteennnii <teennopwijit@gmail.com>
2022-11-30 00:31:54 +0700

    comment

--- memosite/settings.py
+++ memosite/settings.py
@@ -160,4 +160,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
