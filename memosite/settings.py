86f0b3a1517a941d8b7ba359e11700ee22aa9aa9
teenteennnii <teennopwijit@gmail.com>
2022-11-30 00:37:26 +0700

    update

--- memosite/settings.py
+++ memosite/settings.py
@@ -161,3 +161,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 # django_heroku.settings(locals())
+django_heroku.settings(locals())
